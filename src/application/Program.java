ad8a8e9c8b4674a9800b7f006d0b681d7446e56c
PTC <thqv74@gmail.com>
2021-03-29 21:30:11 -0300

    program Class was created including a employee list

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,46 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+import entities.OutSourcedEmployee;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		List<Employee> list = new ArrayList<>();
+		System.out.println("Enter the number of Employees: ");
+		int n = sc.nextInt();
+		String name;
+		int hour;
+		double value;
+		for (int i = 1; i <= n; ++i) {
+			System.out.println("Employee #" + i + " data: ");
+			System.out.println("Outsorced y / n ? :");
+			char sn = sc.nextLine().charAt(0);
+			sc.nextLine();
+			System.out.print("Name :");
+			name = sc.nextLine();
+			System.out.print("Hours :");
+			hour = sc.nextInt();
+			System.out.print("Value per hour: ");
+			value = sc.nextDouble();
+			if (sn == 'y') {
+				System.out.print("Addicional Charge :");
+				double charge = sc.nextDouble();
+				list.add(new OutSourcedEmployee(name, hour, value, charge));
+			} else {
+				list.add(new Employee(name, hour, value));
+			}
+
+		}
+
+		sc.close();
 
 	}
 
